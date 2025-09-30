1220ee38b0730b1a26b28c0b1e86e8717fc92a70
Alex <cartnix21@gmail.com>
2025-09-30 20:54:01 +0300

    Geo hook added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import SelectField from "./components/Select"
+import { useGeolocation } from "./hooks/useGeolocation"
 
 function App() {
+
+  const city = useGeolocation()
+  console.log(city)
+
   return (
     <div className="flex bg-[#b48787] h-dvh justify-center items-center flex-col gap-5">
       <h2 className="text-4xl text-[#E6E6E6] font-bold">Hello, select your city</h2>
---
+++ src/hooks/useGeolocation.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+
+export const useGeolocation = () => {
+  const [coords, setCoords] = useState<{ lat: number; lon: number } | null>(null)
+  const [error, setError] = useState<Error | null>(null)
+  const [isLoading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setCoords({ lat: pos.coords.latitude, lon: pos.coords.longitude })
+        setLoading(false)
+      },
+      (geoError) => {
+        setError(new Error(geoError.message))
+        setLoading(false)
+      }
+    )
+  }, [])
+
+  return { coords, error, isLoading }
+}
