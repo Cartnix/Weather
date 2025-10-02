9096f4539dea07726f84b6fa464c804653c7e330
Alex <cartnix21@gmail.com>
2025-10-02 18:39:01 +0300

    fix: fetch weather data correctly with coordinates

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import SelectField from "./components/Select"
 import useCity from "./hooks/useCity"
+import useWeather from "./hooks/useWeather"
 
 function App() {
 
   const { city, cityErr, isLoading } = useCity()
+  useWeather()
 
   return (
     <div className="flex bg-[#b48787] h-dvh justify-center items-center flex-col gap-5">
       <h2>
         {isLoading && "Определяем город..."}
         {cityErr && `Произошла ошибка: ${cityErr}`}
-        {city && `Город найден: ${city}`}
+        {city && `Погода в городе ${city}: `}
       </h2>
       < SelectField />
     </div>
--- src/hooks/useWeather.tsx
+++ src/hooks/useWeather.tsx
@@ -1,16 +1,35 @@
 import { useEffect, useState } from "react";
-import useCity from "./useCity";
+import { useGeolocation } from "./useGeolocation";
 
-export default function useWeather()
-{
-    const{city} = useCity()
-    const[weather, setWeather] = useState<null>(null)
-    const[err, setErr] = useState<Error | null>(null)
-    const[isLoading, setIsLoading] = useState<boolean>(false)
+export default function useWeather() {
+  const { coords } = useGeolocation();
+  const [weather, setWeather] = useState<any>(null);
+  const [err, setErr] = useState<Error | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const API_KEY = import.meta.env.VITE_API_KEY;
 
-    useEffect(() => {
-        const weather = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=43.238949&lon=76.889709&units=metric&appid=YOUR_API_KEY
-`)
-    }, [city])
+  useEffect(() => {
+    if (!coords) return;
 
+    const fetchWeather = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${coords.lat}&lon=${coords.lon}&appid=${API_KEY}&units=metric&lang=ru`);
+        if (!res.ok) throw new Error("Ошибка при загрузке погоды");
+        const weatherData = await res.json();
+        setWeather(weatherData);
+        console.log(weatherData);
+      } catch (error) {
+        if (error instanceof Error) setErr(error);
+        else setErr(new Error("Неизвестная ошибка"));
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchWeather();
+  }, [coords, API_KEY]);
+
+  return { weather, err, isLoading };
 }
