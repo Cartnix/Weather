15dde8a763fd2d21806355907004b7783704ab57
Alex <cartnix21@gmail.com>
2025-10-12 09:32:57 +0300

    Fix: deploy fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import WeatherMainCard from "./components/WeatherMainCard"
 import WeatherWrapper from "./components/WeatherWrapper"
 import useCity from "./hooks/useCity"
-import useDailyWeather from "./hooks/useDailyWeather"
 
 function App() {
 
   const { city, cityErr, isLoadingCity } = useCity()
-  useDailyWeather()
   return (
-    <div className="flex bg-[#FAFAFA] justify-center items-center flex-col gap-5">
+    <div className="flex bg-[#FAFAFA] justify-center items-center flex-col gap-5 p-20">
       <h2 className="text-3xl font-bold text-[#222222]">
         {isLoadingCity && "Определяем город..."}
         {cityErr && `Произошла ошибка: ${cityErr}`}
--- src/components/MainChart.tsx
+++ src/components/MainChart.tsx
@@ -1,5 +1,12 @@
-import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis, type TooltipProps } from "recharts";
 import useDailyWeather from "../hooks/useDailyWeather";
+
+interface CustomTooltipProps extends TooltipProps<number, string> {
+    payload?: {
+        value: number;
+    }[];
+    label?: string;
+}
 
 export default function MainChart() {
     const { dailyData, error, loading } = useDailyWeather()
@@ -8,12 +15,37 @@ export default function MainChart() {
     if (error) return <p>Произошла ошибка: {error.message}</p>
 
     return (
-        <LineChart width={500} height={300} data={dailyData ?? []}>
-            <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-            <XAxis dataKey="date" />
-            <YAxis />
-            <Line type="monotone" dataKey="temp" stroke="#8884d8" />
-        </LineChart>
+        <ResponsiveContainer width="100%" height={300}>
+            <AreaChart data={dailyData ?? []}>
+                <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+                <XAxis dataKey="date" />
+                <YAxis dataKey="temp" />
+                <Area
+                    type="monotone"
+                    dataKey="temp"
+                    stroke="#8884d8"
+                    fill="#8884d8"
+                    fillOpacity={0.2}
+                    activeDot={false}
+                />
+                <Tooltip content={<CustomTooltip />} />
+            </AreaChart>
+        </ResponsiveContainer>
     )
 
 }
+
+const CustomTooltip: React.FC<CustomTooltipProps> = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+        return (
+            <div className="bg-white/90 backdrop-blur-sm border border-gray-200 rounded-lg p-2 shadow-md">
+                <p className="text-sm text-gray-700">{`Дата: ${label}`}</p>
+                <p className="text-sm text-blue-600 font-semibold">
+                    {`Температура: ${payload[0].value}°C`}
+                </p>
+            </div>
+        );
+    }
+
+    return null;
+};
